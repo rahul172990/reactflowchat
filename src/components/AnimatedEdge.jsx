cd6e9b4ab17699f93a929dfc0ace5c98e29cd709
rahul172990 <rahul172990@gmail.com>
2025-02-12 19:13:06 +0530

    first commit

---
+++ src/components/AnimatedEdge.jsx
@@ -0,0 +1,17 @@
+const AnimatedEdge = ({ id, sourceX, sourceY, targetX, targetY, style }) => {
+  const edgePath = `M ${sourceX},${sourceY} L ${targetX},${targetY}`;
+
+  return (
+    <path
+      id={id}
+      style={style}
+      className="animated-edge"
+      d={edgePath}
+      fill="none"
+      strokeWidth={2}
+      stroke="#ff00cc"
+    />
+  );
+};
+
+export default AnimatedEdge;
